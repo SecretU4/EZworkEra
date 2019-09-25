5b10a917d8f8941e3b961511e29a1ad707239b40
SecretU4 <hero9802@gmail.com>
2019-09-25 15:07:47 +0900

    클래스  메이크 시도 - 인풋부분 클래스 제외

--- TestCode/클래스 메이킹.py
+++ TestCode/클래스 메이킹.py
@@ -1,13 +1,11 @@
 # CSV 정보 추출 코드 (클래스 기반)
-
 # 사용 라이브러리
 import os
 import csv
-
 # 클래스 목록
 ## 파일 관련 입력 정보 클래스
-class File:
-    PresetOn = 0
+class FileInfo:
+    PresetOn = 0 # preset 사용여부 판별
     def __init__(self, Dir, Name, Type):
         self.Dir = Dir
         self.Name = Name
@@ -16,36 +14,61 @@ class File:
         self.DirName = self.Dir + self.Name + '.' + self.Type
         self.FullName = self.Name + '.' + self.Type
         self.PresetOn = 1
+## 각종 정보 필터 클래스
+class DataFilter:
+    def DirSlash(self, Dir):
+        Filter_DirSlash = list(Dir)
+        if list(Dir) == []: pass
+        elif Filter_DirSlash.pop() != "/":
+            Dir = Dir + "/"
+        return Dir
 ## 파일 불러오기 클래스
-class Load:
+class LoadFile:
     def __init__(self, NameDir):
         self.NameDir = NameDir
+    def onlyopen(self):
+        open(self.NameDir)
     def readwrite(self):
         open(self.NameDir, 'w', encoding='UTF-8=sig', newline='')
     def readonly(self):
         open(self.NameDir, 'r', encoding='UTF-8-sig', newline='')
     def addwrite(self):
         open(self.NameDir, 'a', encoding='UTF-8-sig', newline='')
+class CSVLoad(LoadFile):
     def ListCSV(self, info1, info2):
         pass
     def CharaCSV(self, info1, info2):
         pass
     def OtherCSV(self, number, info1):
         pass
-    
+class ERBLoad(LoadFile):
+    pass
 # 사용자 입력 부분
-inputDir = str(input("파일이 위치하는 폴더명을 입력해주세요. : "))
-if inputDir.find('/')!=len(inputDir)-1:
-    inputDir = inputDir + '/'
-inputName = str(input("파일의 이름을 입력해주세요. : "))
-inputType = str(input("파일의 타입을 입력해주세요. 현재 CSV 타입만 지원합니다.: ")).upper()
-if inputType != 'CSV':
-    print("안된다니까요")
-InputA = File(inputDir, inputName, inputType)
+inputDir = str(input("파일이 위치하는 폴더명을 입력해주세요. \
+존재하지 않는 폴더명인 경우 오류가 발생합니다.: "))
+if len(inputDir)==0:
+    print("특정 디렉토리의 입력 없이 진행합니다.")
+inputDir = DataFilter().DirSlash(inputDir)
+while True:
+    inputName = str(input("파일의 이름을 입력해주세요. : "))
+    if len(inputName) == 0:
+        print("공란은 입력하실 수 없습니다.")
+        continue
+    break
+while True:
+    inputType = str(input("파일의 타입을 입력해주세요. 현재 CSV 타입만 지원합니다.: ")).upper()
+    if len(inputType) == 0:
+        print("공란은 입력하실 수 없습니다.")
+        continue
+    elif inputType == 'CSV': print("땡큐!")
+    else: print("안돼도 모릅니다.")
+    break
+InputA = FileInfo(inputDir, inputName, inputType)
 InputA.SetPreset()
-print("여기까진 됨")
 if InputA.PresetOn == 1:
-    print(InputA.DirName)
-    print("it was true")
+    print("체크 해냈다!")
 elif InputA.PresetOn == 0:
-    print("프리셋 처리 안됨")
+    print("슈벌")
+print(InputA.DirName)
+TestA = FileInfo('TestDir', 'TestNam', 'TestTyp')
+print(TestA.PresetOn)
