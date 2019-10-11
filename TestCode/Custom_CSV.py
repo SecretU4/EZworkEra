f831b2b5eabb706ef200e2213e14fb990462c40a
SecretU4 <hero9802@gmail.com>
2019-10-11 20:03:43 +0900

    CSV 크롤링 기능 custom_CSV 모듈로 이관. 앞으로도 이렇게 할것.

--- TestCode/Custom_CSV.py
+++ TestCode/Custom_CSV.py
@@ -1,5 +1,5 @@
 # CSV 기능 관련 모듈
-from Custom_Main import LoadFile, DataFilter
+from Custom_Main import LoadFile, DataFilter, Common_Sent, Menu, Menu_Preset, Custom_input, LoadFile, Find_File_in_Dir
 import csv
 class CSVLoad(LoadFile):
     def CSVRun(self):
@@ -35,6 +35,33 @@ class CSVLoad(LoadFile):
             InfoDATA_list.append(DATA_2)
         InfoCSV_DATA = DataFilter().Dup_Filter(InfoDATA_list)
         return InfoCSV_DATA
-class CSVModule: # 현재 미실장.
-    def ExportCSV(self):
-        pass
+class CSV_FuncModule:
+    def Import_All_CSV(self):
+        print("추출을 시작합니다.")
+        DebugTXT = LoadFile('debug.txt','UTF-8').readwrite()
+        DebugLog = LoadFile('debuglog.txt','UTF-8').readwrite()
+        DebugLog.write("오류코드 0xef는 UTF-8-sig, 다른 경우 cp932(일본어),cp949(한국어)로 시도하세요.\n")
+        ErrorCheck, FileCount = 0,0
+        CSV_User_input = Custom_input("CSV")
+        Target_Dir_CSV = CSV_User_input.InputOption(1)
+        Encode_Type = Menu_Preset().Encode()
+        CSVFile_List = Find_File_in_Dir().FileList_Ext(Target_Dir_CSV,'.CSV')
+        for filename in CSVFile_List:
+            CSVOpenFile = CSVLoad(filename,Encode_Type)
+            with CSVOpenFile.readonly():
+                try:
+                    CSVOpenFile.CoreCSV(0,0)
+                except UnicodeDecodeError as UniDecode:
+                    print("{}에서 {} 발생\n".format(filename,UniDecode),file=DebugLog)
+                    ErrorCheck += 1
+            CSVOpenFile.DataDic = DataFilter().EraseQuote(CSVOpenFile.DataDic,';')
+            print('{0}\n{1}\n'.format(filename,CSVOpenFile.DataDic),file=DebugTXT)
+            FileCount += 1
+        if ErrorCheck != 0:
+            print("{}건 추출 도중 {}건의 인코딩 오류가 발생했습니다. debuglog.txt를 확인해주세요.".format(FileCount,ErrorCheck))
+        else:
+            print("{}건이 추출되었습니다.".format(FileCount))
+            DebugLog.write("오류가 발생하지 않았거나 덮어씌워졌습니다.")
+        DebugTXT.close
+        DebugLog.close
+        Common_Sent.ExtractFinish()
--- TestCode/Mainstream.py
+++ TestCode/Mainstream.py
@@ -1,44 +1,29 @@
 # EmuEra용 번역 파일 처리/합병 툴
 # 사용되는 라이브러리
-from Custom_Main import Common_Sent, Menu, Menu_Preset, Custom_input, LoadFile, Find_File_in_Dir, DataFilter
-from Custom_CSV import CSVLoad
+from Custom_Main import Common_Sent, Menu
+from Custom_CSV import CSVLoad, CSV_FuncModule
 # from Custom_ERB import ERBLoad, ERBWrite
 Menu_Main_Dict = {0:'CSV 파일 처리',1:'ERB 파일 처리 (미실장)',2:'ERH 파일 처리 (미실장)',3:'프로그램 종료'}
 MainMenu = Menu(Menu_Main_Dict)
 MainMenu.Title("EZworkEra - Develop utility for EmuEra base game")
 while True:
+    print("작업 후 버튼을 눌러 프로그램을 종료하셔야 작업파일이 손실되지 않습니다.")
+    Common_Sent.PrintLine
     MainMenu.Run_Menu()
-    # [1] CSV 파일의 처리
+# [1] CSV 파일의 처리
     if MainMenu.MenuSelect == 0:
         print("CSV 파일 처리 유틸리티입니다.")
-        DebugLog = LoadFile('debuglog.txt','UTF-8').readwrite()
-        DebugLog.write("오류코드 0xef는 UTF-8-sig, 다른 경우 cp932(일본어),cp949(한국어)로 시도하세요.\n")
-        DebugTXT = LoadFile('debug.txt','UTF-8').readwrite()
-        CSV_User_input = Custom_input("CSV")
-        Target_Dir_CSV = CSV_User_input.InputOption(1)
-        Encode_Type = Menu_Preset().Encode()
-        CSVFile_List = Find_File_in_Dir().FileList_Ext(Target_Dir_CSV,'.CSV')
-        for filename in CSVFile_List:
-            CSVOpenFile = CSVLoad(filename,Encode_Type)
-            with CSVOpenFile.readonly() as LoadedCSV:
-                try:
-                    CSVOpenFile.CoreCSV(0,0)
-                except UnicodeDecodeError as UniDecode:
-                    print("{}에서 {} 발생\n".format(filename,UniDecode),file=DebugLog)
-            CSVOpenFile.DataDic = DataFilter().EraseQuote(CSVOpenFile.DataDic,';')
-            print('{0}\n{1}\n'.format(filename,CSVOpenFile.DataDic),file=DebugTXT)
-        DebugTXT.close
-        Common_Sent.ExtractFinish()
+        CSV_FuncModule().Import_All_CSV()
         MainMenu.MenuSelect = 0
-    # [2] ERB 파일의 처리
+# [2] ERB 파일의 처리
     elif MainMenu.MenuSelect == 1:
         print("미실장입니다.")
         MainMenu.MenuSelect = 0
-    # [3] ERH 파일의 처리
+# [3] ERH 파일의 처리
     elif MainMenu.MenuSelect == 2:
         print("미실장입니다")
         MainMenu.MenuSelect = 0
-    # [4] 프로그램 종료
+# [4] 프로그램 종료
     elif MainMenu.MenuSelect == 3:
         break
 Common_Sent.EndComment()
