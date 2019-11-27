421b7a46335cce4ce1aadea3108fc72525a0278d
SecretU4 <hero9802@gmail.com>
2019-11-27 10:00:09 +0900

    이전 커밋에서 docstring을 작성하지 않은 일부 모듈의 docstring 추가

--- Core/customdb.py
+++ Core/customdb.py
@@ -5,22 +5,65 @@ Classes:
     ERBMetaInfo
 """
 
-class InfoDict: # {파일명:{파일내 정보 딕셔너리형}}
+class InfoDict:
+    """코드 내에서 각 자료명을 파일명으로 분류해야 하는 경우 사용하는 자료형 클래스
+
+    Functions:
+        add_dict(dictname,data)
+        make_dictvals_list()
+    Variables:
+        dict_main
+            클래스 내 기록된 dict형 자료. {dictname:data} 형식
+        db_ver
+            기록 양식 확인용 클래스 버전
+        dict_name_dictvals
+            data가 dict형인 경우 함수 호출을 통해 사용 가능한 변수.\n
+            {dictname:data.values()} 형식
+    """
     def __init__(self):
         self.dict_main = {}
+        self.dict_name_dictvals = {}
 
     db_ver = '1.1'
 
     def add_dict(self,dictname,dataname):
+        """클래스 내 자료형에 새로운 정보 추가."""
         self.dict_main[dictname]=dataname
 
-    def make_dictvals_list(self): # {파일명:[파일내 정보 딕셔너리.values()]}
-        self.dict_name_dictvals = {}
+    def make_dictvals_list(self):
+        """클래스 내 자료형 안의 data 중 dict형에 대해
+        해당 dict의 value 값 list를 추출해 새로운 dict를 만듦.\n
+        dict형이 아닌 data가 있을 시 작업 완료 후 메세지를 출력함.
+        """
+        error_count = 0
         for name in list(self.dict_main):
-            self.dict_name_dictvals[name]=list(self.dict_main[name].values())
+            try:
+                self.dict_name_dictvals[name]=list(self.dict_main[name].values())
+            except AttributeError:
+                error_count += 1
+        if error_count != 0:
+            print("작업 도중 dict형이 아닌 자료형이 확인되었습니다.")
 
 
-class ERBMetaInfo: # [조건문단계,경우단계,경우수,해당 줄]
+class ERBMetaInfo:
+    """ERB 파일 처리에 사용되는 자료형 클래스.
+    작업시 각 line 처리시마다 level 변수와 count 변수의 처리가 필요함.
+
+    Functions:
+        add_line_list(str)
+    Variables:
+        db_ver
+            기록 양식 확인용 클래스 버전
+        linelist
+            작업 완료시 결과 list. [[IF단계,CASE단계,CASE수,line]] 형식
+        if_level
+            IF문의 깊이 판단 변수
+        case_level
+            CASE/DATAFORM문의 깊이 판단 변수
+        case_count
+            CASE/DATAFORM문의 개수 판단 변수.
+            CASE/DATAFORM 내 CASE/DATAFORM이 포함된 경우 정확하지 않을 수 있음.
+    """
     def __init__(self):
         self.linelist = []
         self.if_level = 0
@@ -30,4 +73,5 @@ class ERBMetaInfo: # [조건문단계,경우단계,경우수,해당 줄]
     db_ver = '1.1'
 
     def add_line_list(self,line):
+        """linelist에 새로운 line 정보 추가"""
         self.linelist.append([self.if_level,self.case_level,self.case_count,line])
--- Core/util.py
+++ Core/util.py
@@ -1,4 +1,10 @@
-# EZworkEra 공용 모듈
+"""내부 코드에서 공용으로 사용되는 기능 클래스 모듈.
+
+Classes:
+    CommonSent
+    DataFilter
+    MakeLog
+"""
 # 사용 라이브러리
 import os
 import pickle
@@ -7,8 +13,17 @@ from customdb import InfoDict
 from usefile import LoadFile
 from System.interface import KoreanSupport
 # 클래스 목록
-## 사용자 화면 관련
-class CommonSent: #관용구 클래스
+class CommonSent:
+    """자주 사용되는 관용구 묶음 클래스. 모든 함수는 staticmethod로, 클래스의 self를 선언하지 않는다.
+
+    Functions:
+        not_ok()
+        end_comment()
+        no_void()
+        print_line()
+        extract_finished()
+        put_time()
+    """
     @staticmethod
     def not_ok():
         print("유효하지 않은 입력입니다.")
@@ -26,6 +41,7 @@ class CommonSent: #관용구 클래스
 
     @staticmethod
     def print_line():
+        """구분선 생성"""
         print("".center(100,"="))
 
     @staticmethod
@@ -35,11 +51,19 @@ class CommonSent: #관용구 클래스
 
     @staticmethod
     def put_time():
+        """호출시의 시간 출력"""
         return time.strftime('%Y/%m/%d %H:%M',time.localtime(time.time()))
 
 
 class DataFilter:
-    def dup_filter(self, list_name): #중복 감별 들어온 순서대로 저장
+    """주어진 데이터를 처리하는 클래스
+
+    Functions:
+        dup_filter(list)
+        erase_quote(data,quotechar)
+    """
+    def dup_filter(self, list_name):
+        """list 내에서 중복되는 요소를 제거한 후 다시 list로 돌려줌."""
         dup_filtered = []
         set_filter = set()
         for data in list_name:
@@ -49,6 +73,7 @@ class DataFilter:
         return dup_filtered
 
     def erase_quote(self,dataname,quotechar):
+        """입력받은 data(dict,list,str) 내 각 요소 중 quotechar 인자가 포함된 부분을 지움."""
         if isinstance(dataname,dict) is True:
             dict_erased = {}
             for key in list(dataname.keys()):
@@ -73,7 +98,19 @@ class DataFilter:
 
 
 class MakeLog(LoadFile):
+    """작업 절차 중 log파일에 대한 클래스. 클래스 호출시 logfile명과 사용될 인코딩을 입력해야 한다.
+
+    Functions:
+        first_log(file_info)
+        write_log(str)
+        write_error_log(error_code,target)
+        write_loaded_log(filename)
+    Variables:
+        NameDir
+            log파일 명칭
+    """
     def first_log(self,file_info=None):
+        """작업 시작 절차를 logfile에 기록함. 입력받은 인자가 없으면 시작 시간만을 입력함."""
         with self.addwrite() as log_open:
             if file_info == None:
                 log_open.write('\n{} 실행됨\n'.format(CommonSent.put_time))
@@ -81,16 +118,23 @@ class MakeLog(LoadFile):
                 log_open.write('\n{}\n{} 불러오기 성공.\n'.format(CommonSent.put_time,file_info))
 
     def write_log(self,line='Defaultline'):
+        """입력받은 str 타입 자료형을 logfile에 기록함.\n
+        인자 미입력시 'DefaultLine'이 입력됨.
+        """
         with self.addwrite() as log_open:
             log_open.write('{}'.format(line))
 
     def write_error_log(self,error_code,target=None):
+        """입력받은 error 와 해당 위치(target)를 양식에 맞게 logfile에 기록함.\n
+        target은 선택사항이고, 해당 경우 error_code만 입력됨.
+        """
         with self.addwrite() as log_open:
             log_open.write('{} 오류 발생!'.format(error_code))
             if target != None:
                 log_open.write('발생 위치: {}'.format(target))
 
     def write_loaded_log(self,filename):
+        """작업 중 로드된 파일을 양식에 맞게 logfile에 기록함."""
         with self.addwrite() as log_open:
             log_open.write("파일명: {}".format(filename))
 
