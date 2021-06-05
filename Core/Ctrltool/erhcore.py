57e753fe8cbce44a3c8cccfb3273f40f756fd220
SecretU4 <53875534+SecretU4@users.noreply.github.com>
2021-06-05 13:48:27 +0900

    HandleDIM 작동방식 개선

--- Core/Ctrltool/erhcore.py
+++ Core/Ctrltool/erhcore.py
@@ -10,10 +10,11 @@ class ERHLoad(LoadFile):
 
 
 class HandleDIM:
-    def __init__(self, dim_dict=dict()):
+    def __init__(self, dim_dict: dict[str, list] = dict()):
         self.dim_dict = dim_dict
 
-    def __del_dim_mid(self, words):  # TODO 호출자 따른 변동 가능하도록
+    def __del_dim_mid(self, words:list[str]) -> list[str]:  # TODO 호출자 따른 변동 가능하도록
+        """DIM 관련 중간 식별자 제거 함수"""
         dim_mid = ("#DIM", "#DIMS", "DYNAMIC", "CONST", "SAVEDATA", "CHARADATA", "GLOBAL", "REF")
         temp = words.copy()
 
@@ -23,12 +24,12 @@ class HandleDIM:
             temp.pop(0)
             return self.__del_dim_mid(temp)
 
-    def dim_search(self, line):  # TODO DIM 분석 필요
-        is_defined = 0
-        array_list = []  # 차원수
-        defined_list = []  # 초기값수
-        result = []
-        temp_dimdict = dict()
+    def dim_search(self, line: str):  # TODO DIM 작동방식 추가분석 필요
+        is_defined = False
+        array_list: list[str] = []  # 차원수
+        defined_list: list[str] = []  # 초기값수
+        result: list[str] = []
+        temp_dimdict: dict[str, result] = dict()
 
         words = line.split()
         if not line.startswith("#DIM"):
@@ -40,23 +41,24 @@ class HandleDIM:
         for word in words:
             word = word.replace(",", "").strip()
             if word in ("="):
-                is_defined = 1
+                is_defined = True
             elif is_defined:
                 defined_list.append(word)
             elif word.isdecimal():
                 array_list.append(word)
 
         if array_list:  # 다차원 지원
-            if len(array_list) == 1:  # 1차원
+            len_array = len(array_list)
+            if len_array == 1:  # 1차원
                 for count in range(array_list[0]):
-                    result.append(0)
+                    result.append("0")
             else:
-                print("배열 관련은 아직 실장되지 않았습니다.")
+                print("2차원 이상의 배열은 현재 지원하지 않습니다. 현재: %d차원" % len_array)
         elif defined_list:  # 초기값 있을때
             for count, arg in enumerate(defined_list):
                 result.insert(count, arg)
         else:  # 초기값 미설정
-            result.append(0)
+            result.append("0")
 
         temp_dimdict[dim_name] = result
         self.dim_dict.update(temp_dimdict)
